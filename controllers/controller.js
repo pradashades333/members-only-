b957b8bf446033917afb0df46d16a08be3115c38
pradashades333 <alexdreve03@gmail.com>
2026-03-15 23:01:56 +0100

    added more controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,11 +16,31 @@ module.exports = {
         res.render("auth/sign-up")
     },
 
-    signUpPost: async(req,res) => {
-        const { first_name, last_name, email, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10)
-        await pool.query(`INSERT INTO "user" (first_name, last_name, email, password) VALUES ($1,$2,$3,$4)`, [first_name, last_name, email, hashedPassword])
-        res.redirect("/log-in")
+    signUpPost: async(req,res, next) => {
+        try {
+            const { first_name, last_name, email, password } = req.body;
+            const hashedPassword = await bcrypt.hash(password, 10)
+            await pool.query(`INSERT INTO "user" (first_name, last_name, email, password) VALUES ($1,$2,$3,$4)`, [first_name, last_name, email, hashedPassword])
+            res.redirect("/log-in")
+        } catch (err) {
+            next(err)
+        }
+    },
+
+    logInGet: async(req,res) => {
+        res.render("auth/log-in")
+    },
+
+    logInPost: passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/log-in"
+    }),
+
+    logOut:async (req,res, next) => {
+        req.logout((err) => {
+        if (err) return next(err);
+        res.redirect("/");
+        });
     }
 
 }
