c9c6466f65d52bc478341beaea434e32221f5eb3
pradashades333 <alexdreve03@gmail.com>
2026-03-16 15:11:54 +0100

    fixed issues in app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const session = require("express-session");
 const passport = require("passport");
 require("./db/passport");
-const indexRouter = require("./routes/index");
-app.use("/", indexRouter);
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +22,10 @@ app.use(passport.session());
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
-})
+});
+
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
